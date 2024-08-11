be631bd7b72b9b8d4c6c8f921d7e092d5c565f92
Anatoly-Ivanov8008 <ivanov270880@gmail.com>
2024-08-11 10:18:21 +0800

    module_7_5
    """"
    Модуль 7 задание 5
    Домашнее задание по теме "Файлы в операционной системе".
    """""

---
+++ modul_7/module_7_5.py
@@ -0,0 +1,19 @@
+""""
+Модуль 7 задание 5
+Домашнее задание по теме "Файлы в операционной системе".
+"""""
+
+import os
+import time
+
+directory = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(directory, file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(os.path.abspath(file))
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+              f'Родительская директория: {parent_dir}')
